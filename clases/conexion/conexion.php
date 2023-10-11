85bd5c32d90e54edf2c63feda6f6b5a11eb0e777
heitorcto <heitorcasella@gmail.com>
2023-10-11 19:55:05 -0300

    refactor: refatora a classe de conexão utilizando o .env deixando o código mais limpo e legível

--- clases/conexion/conexion.php
+++ clases/conexion/conexion.php
@@ -1,36 +1,21 @@
 <?php
-class  conexion
+
+include 'Env.php';
+
+class Conexion
 {
-    private $server;
-    private $user;
-    private $password;
-    private $database;
-    private $port;
     private $conexion;
 
     function __construct()
     {
-        $listadatos = $this->datosConexion();
-        foreach ($listadatos as $key => $value) {
-            $this->server = $value['server'];
-            $this->user = $value['user'];
-            $this->password = $value['password'];
-            $this->database = $value['database'];
-            $this->port = $value['port'];
-        }
+        $env = Env::getEnvData();
 
-        $this->conexion = new mysqli($this->server, $this->user, $this->password, $this->database, $this->port);
+        $this->conexion = new mysqli($env['DB_SERVER'], $env['DB_USERNAME'], $env['DB_PASSWORD'], $env['DB_DATABASE'], $env['DB_PORT']);
+
         if ($this->conexion->connect_errno) {
             echo "Algo va mal con la conexion";
             die();
         }
-    }
-
-    private function datosConexion()
-    {
-        $direccion =  dirname(__FILE__);
-        $jsondata = file_get_contents($direccion . "/" . "config");
-        return json_decode($jsondata, true);
     }
 
     private function convertirUTF8($array)
